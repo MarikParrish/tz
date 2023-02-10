fd1d4ffa32822b2bb4607a0d26fcb702480a2653
Igor <katsuba96@gmail.com>
2023-02-10 15:05:09 +0200

    added dark theme

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,21 @@
 import './app.scss';
 
 import Table from '../Table/Table';
+import { useState } from 'react';
 
 function App() {
+  
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () =>{
+    setTheme((curr)=>(curr === 'light' ? 'dark' : 'light'));
+  }
+
+  
   return (
-    <>
-      <Table/>
-    </>
+      <div id={theme}>
+        <Table toggleTheme={toggleTheme} />
+      </div>
   );
 }
 
